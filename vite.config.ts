1aabb89109e6507f1ca2d900d4dc77468ca5abdf
STALIN <89456579+stalin5@users.noreply.github.com>
2025-11-18 00:38:44 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/nli8mentor.in/final_dep/",
+  base: "/final_dep/",
   server: {
     host: "::",
     port: 8080,
